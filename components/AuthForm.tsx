e35aefeda9e4f5b4f0a01af713490dcad59d088f
LoickBck <loickbuck@hotmail.com>
2025-02-02 23:00:44 +0100

    login/register

---
+++ components/AuthForm.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  Path,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constant";
+import ImageUpload from "@/components/ImageUpload";
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Bon retour sur Pagino" : "Créer votre compte Pagino"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Accédez à l'immense collection de ressources et restez informé"
+          : "Veuillez remplir tous les champs et télécharger une carte d'étudiant valide pour accéder à la bibliothèque"}
+      </p>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    {field.name === "universityCard" ? (
+                      <ImageUpload />
+                    ) : (
+                      <Input
+                        required
+                        type={
+                          FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]
+                        }
+                        {...field}
+                        className="form-input"
+                      />
+                    )}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button type="submit" className="form-btn">
+            {isSignIn ? "Connexion" : "Enregistrement"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "Nouveau sur Pagino? " : "Déjà un compte? "}
+        <Link
+          href={isSignIn ? "/inscription" : "/connexion"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Créer un compte" : "Se connecter"}
+        </Link>
+      </p>
+    </div>
+  );
+};
+export default AuthForm;
