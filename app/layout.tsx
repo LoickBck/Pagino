aea699a856e97630625e7b6a759330690483b1f8
LoickBck <loickbuck@hotmail.com>
2025-02-06 21:07:38 +0100

    fix deploiement

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,26 +8,10 @@ import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
   src: [
-    {
-      path: require("./fonts/IBMPlexSans-Regular.ttf"),
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: require("./fonts/IBMPlexSans-Medium.ttf"),
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: require("./fonts/IBMPlexSans-Semibold.ttf"),
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: require("./fonts/IBMPlexSans-Bold.ttf"),
-      weight: "700",
-      style: "normal",
-    },
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Semibold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
